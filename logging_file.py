0d5209c014c46c07400d441b2b9840a8744233fd
Lucas64000 <84921615+Lucas64000@users.noreply.github.com>
2024-12-26 10:10:55 +0100

    Init preprocessing logger function

---
+++ logging_file.py
@@ -0,0 +1,12 @@
+import logging
+
+def init_preprocess_logger():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        filename='logs/preprocessing.log',
+        level=logging.INFO,
+        style="{",
+        format="{asctime} - {levelname} - {message}",
+        datefmt="%Y-%m-%d %H:%M",
+    )
+    return logger
